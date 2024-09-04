2f76172e163a845efbc25eeb9c984e27dd8cb0d6
Idowuoj <104337069+Idowuoj@users.noreply.github.com>
2024-09-04 18:04:40 +0100

    this is my new changes

--- capstone-main - Copy/README.md
+++ capstone-main - Copy/README.md
@@ -1,1 +1,3 @@
 # capstone
+**Lets see how this works**
+*I trust it will be good*
---
+++ capstone-main - Copy/tryPHP.php
@@ -0,0 +1,5 @@
+<?php
+  date_default_timezone_set('America/New_York'); // Set your timezone
+  $currentTime = date('h:i:s a'); // Get current time
+  echo "<p>Hello! The current time is $currentTime.</p>";
+?>
