425137aff67fbacecea46f2e1186751874015305
Idowuoj <104337069+Idowuoj@users.noreply.github.com>
2024-09-04 16:40:19 +0100

    the PHP file has no correlation. I might remoe it later, just testing

---
+++ README.md
@@ -0,0 +1,5 @@
+# capstone
+**I don't know how it works**
+1. usin gt to test git pull
+2. it must work
+3. what will haappen i f i push this?
---
+++ testPHP.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Current Date and Time</title>
+</head>
+
+<body>
+  <h1>Current Date and Time:
+    <?php echo date('Y-m-d H:i:s'); ?>
+  </h1>
+</body>
+
+</html>
